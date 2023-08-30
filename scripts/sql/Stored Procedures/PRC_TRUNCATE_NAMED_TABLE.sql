f78641e421969c577333575e88585d764272faa6
Rizaul.Kamal <Rizaul.Kamal@mandg.com>
2023-08-30 09:22:51 +0000

    Merged PR 92054: GUP-589 [Guppy] Updated PRC_TRUNCATE_NAMED_TABLE.sql
    **What does this PR do?**
    Added CRM_PLAT_DYN_ACTIVITYPARTY_STG to the process
    
    ---
    
    **What are the relevant tickets?**
    
    [GUP-589 ](https://jira-mandg.valiantys.net/browse/GUP-589 )
    
    ---
    
    **How has this been tested?**
    Tested in development

--- scripts/sql/Stored Procedures/PRC_TRUNCATE_NAMED_TABLE.sql
+++ scripts/sql/Stored Procedures/PRC_TRUNCATE_NAMED_TABLE.sql
@@ -4,7 +4,7 @@ create procedure STAGE_CDI.PRC_TRUNCATE_NAMED_TABLE
 Author       : Rizaul Kamal
 Date         : 27/04/2022
 Updated By   : Nikita Varnwal
-Last Updated : 23/08/2023
+Last Updated : 30/08/2023
 
 Description: Truncate table in the STAGE_CDI schema (only tables listed can be truncated)
 
@@ -31,8 +31,8 @@ Input      : TABLE_NAME - The name of the table to be truncated
              CRM_PLAT_DYN_CASECLNTMASTR_STG
              CRM_PLAT_DYN_CLIENTMASTR_STG
              CRM_PLAT_DYN_DIVORCECLAIM_STG
-			 CRM_PLAT_DYN_TRANSFEROUT_STG 
-			 CRM_PLAT_DYN_ACCOUNT_STG
+             CRM_PLAT_DYN_TRANSFEROUT_STG 
+             CRM_PLAT_DYN_ACCOUNT_STG
 
 -------------------------------------------------------------------------------
 Version   Date        Description of change
@@ -42,6 +42,7 @@ Version   Date        Description of change
 0.3       29/07/2023  Amend table name CRM_PLAT_DYN_CASECLNTMASTR_STG to CRM_PLAT_DYN_CASECLNTMASTER_STG
 0.4       21/08/2023  Amend table name CRM_PLAT_DYN_TRANSFEROUT_STG
 0.5       23/08/2023  Amend table name CRM_PLAT_DYN_ACCOUNT_STG
+0.6       30/08/2023  Amend table name CRM_PLAT_DYN_ACTIVITYPARTY_STG
 
 */
 with encryption, execute as 'CDI_ADF_TRUNCATE_USER'
@@ -58,26 +59,27 @@ begin -- proc
   if @p_table_name in ('SFDC_ALERTS_STG', 
                        'SFDC_PLAT_MSMGRTN_PARENT_CASE_STG',
                        'CRM_PLAT_DYN_DEATHCLAIM_STG',
-             		   'CRM_PLAT_DYN_ANNOTATION_STG',
-             		   'CRM_PLAT_DYN_EMAILMESSAGE_STG',
-             		   'CRM_PLAT_DYN_INCIDENT_STG',
-             		   'SFDC_PLAT_DYN_ACTIVITY_POINTER_STG',
-             		   'SFDC_PLAT_DYN_EMAIL_STG',
-             		   'SFDC_PLAT_MSMGRTN_CASEFEED_STG',
-             		   'CRM_PLAT_DYN_CONTRIBUTION_STG',
-             		   'CRM_PLAT_DYN_DEMATACTIVITY_STG',
-             		   'CRM_PLAT_DYN_DRAWDOWN_STG',
-             		   'CRM_PLAT_DYN_NONADVSWRKITEM_STG',
-             		   'CRM_PLAT_DYN_PAYMENT_STG',
-             		   'CRM_PLAT_DYN_TRNSFRINACTVTY_STG',
-             		   'CRM_PLAT_DYN_SRC_ACTVTYPNTR_STG',
-             		   'CRM_PLAT_DYN_APAWORKITEM_STG',
-             		   'CRM_PLAT_DYN_ATTACHMENT_STG',
-             		   'CRM_PLAT_DYN_CASECLNTMASTER_STG',
-             		   'CRM_PLAT_DYN_CLIENTMASTR_STG',
-             		   'CRM_PLAT_DYN_DIVORCECLAIM_STG',
-					   'CRM_PLAT_DYN_TRANSFEROUT_STG',
-					   'CRM_PLAT_DYN_ACCOUNT_STG')
+                       'CRM_PLAT_DYN_ANNOTATION_STG',
+                       'CRM_PLAT_DYN_EMAILMESSAGE_STG',
+                       'CRM_PLAT_DYN_INCIDENT_STG',
+                       'SFDC_PLAT_DYN_ACTIVITY_POINTER_STG',
+                       'SFDC_PLAT_DYN_EMAIL_STG',
+                       'SFDC_PLAT_MSMGRTN_CASEFEED_STG',
+                       'CRM_PLAT_DYN_CONTRIBUTION_STG',
+                       'CRM_PLAT_DYN_DEMATACTIVITY_STG',
+                       'CRM_PLAT_DYN_DRAWDOWN_STG',
+                       'CRM_PLAT_DYN_NONADVSWRKITEM_STG',
+                       'CRM_PLAT_DYN_PAYMENT_STG',
+                       'CRM_PLAT_DYN_TRNSFRINACTVTY_STG',
+                       'CRM_PLAT_DYN_SRC_ACTVTYPNTR_STG',
+                       'CRM_PLAT_DYN_APAWORKITEM_STG',
+                       'CRM_PLAT_DYN_ATTACHMENT_STG',
+                       'CRM_PLAT_DYN_CASECLNTMASTER_STG',
+                       'CRM_PLAT_DYN_CLIENTMASTR_STG',
+                       'CRM_PLAT_DYN_DIVORCECLAIM_STG',
+                       'CRM_PLAT_DYN_TRANSFEROUT_STG',
+                       'CRM_PLAT_DYN_ACCOUNT_STG',
+                       'CRM_PLAT_DYN_ACTIVITYPARTY_STG')
   begin
     begin try
       set @v_sql = 'TRUNCATE TABLE STAGE_CDI.' + @p_table_name
